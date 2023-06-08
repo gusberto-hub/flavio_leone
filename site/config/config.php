68a0d429de039fa799409634687a8c504a7d3340
gdiguglielmo <gusbertus@gmail.com>
2023-06-08 11:30:38 +0200

    updated plugins

--- site/config/config.php
+++ site/config/config.php
@@ -1,63 +1,61 @@
 <?php
 
 return [
-'debug' => false,
+    'debug' => false,
 
-'routes' => [
-    [
-        'pattern' => 'sitemap.xml',
-        'action'  => function() {
-            $pages = site()->pages()->index();
-  
-            // fetch the pages to ignore from the config settings,
-            // if nothing is set, we ignore the error page
-            $ignore = kirby()->option('sitemap.ignore', ['error','about','projects/commissions']);
-  
-            $content = snippet('sitemap', compact('pages', 'ignore'), true);
-  
-            // return response with correct header type
-            return new Kirby\Cms\Response($content, 'application/xml');
-        }
-      ],
-      [
-        'pattern' => 'sitemap',
-        'action'  => function() {
-          return go('sitemap.xml', 301);
-        }
-    ],
-    [
-        'pattern' => 'projects/personal/(:all)',
-        'action' => function($uid) {
-        go($uid);
-        }
-    ],
-    [
-        'pattern' => 'projects/commissions/(:all)',
-        'action' => function($uid) {
-        go($uid);
-        }
-    ],
-    [
-        'pattern' => 'projects/index/(:all)',
-        'action' => function($uid) {
-        go($uid);
-        }
-    ],
-    [
-        'pattern' => '(:any)(:all)',
-        'action' => function($u1,$u2) {
-        $page = page($u1.$u2);
-        
-        if(!$page) $page = page('projects/personal/' . $u1.$u2);
-        if(!$page) $page = page('projects/commissions/' . $u1.$u2);
-        if(!$page) $page = page('projects/index/' . $u1.$u2);
-        if(!$page) $page = site()->errorPage();
-        
-        return site()->visit($page);
-        }
-    ],
-  ]
+    'routes' => [
+        [
+            'pattern' => 'sitemap.xml',
+            'action'  => function () {
+                $pages = site()->pages()->index();
+
+                // fetch the pages to ignore from the config settings,
+                // if nothing is set, we ignore the error page
+                $ignore = kirby()->option('sitemap.ignore', ['error', 'about', 'projects/commissions']);
+
+                $content = snippet('sitemap', compact('pages', 'ignore'), true);
+
+                // return response with correct header type
+                return new Kirby\Cms\Response($content, 'application/xml');
+            }
+        ],
+        [
+            'pattern' => 'sitemap',
+            'action'  => function () {
+                return go('sitemap.xml', 301);
+            }
+        ],
+        [
+            'pattern' => 'projects/personal/(:all)',
+            'action' => function ($uid) {
+                go($uid);
+            }
+        ],
+        [
+            'pattern' => 'projects/commissions/(:all)',
+            'action' => function ($uid) {
+                go($uid);
+            }
+        ],
+        [
+            'pattern' => 'projects/index/(:all)',
+            'action' => function ($uid) {
+                go($uid);
+            }
+        ],
+        [
+            'pattern' => '(:any)(:all)',
+            'action' => function ($u1, $u2) {
+                $page = page($u1 . $u2);
+
+                if (!$page) $page = page('projects/personal/' . $u1 . $u2);
+                if (!$page) $page = page('projects/commissions/' . $u1 . $u2);
+                if (!$page) $page = page('projects/index/' . $u1 . $u2);
+                if (!$page) $page = site()->errorPage();
+
+                return site()->visit($page);
+            }
+        ],
+    ]
 
 ];
-
-?>
